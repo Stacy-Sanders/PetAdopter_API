7824f4b9a93ee237e5ec2e672f3d3063af7a6421
jorwood1998 <jorwood1998@gmail.com>
2022-01-31 11:37:26 -0500

    shelter service

---
+++ PetAdopter_API.Models/Shelter/ShelterDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetAdopter_API.Models
+{
+    public class ShelterDetail
+    {
+        public int ShelterId { get; set; }
+        public string ShelterName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public float Rating { get; set; }
+
+    }
+}
